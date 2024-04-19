0021756163507cf0dab1d9acd2d087ce13413a6f
Clestar <clestar73@gmail.com>
2024-04-19 15:12:19 +0900

    comment page 변경시에도 댓글 추출 bug)댓글과 대댓글이 다른 페이지일시 대댓글 추출 x

--- scripts/dcinside.js
+++ scripts/dcinside.js
@@ -1,21 +1,42 @@
-var elements = document.querySelectorAll('li[id^="comment_li_"]');
-elements.forEach(function(element) {
-    var pElement = element.querySelector("p");
-    if(pElement != null){
-        console.log(pElement.textContent);
-        var reply="reply_list"+element.id.substr(10);
-        var re_Element = document.querySelector("#"+reply);
-        if(re_Element!=null){
-            re_Element = re_Element.querySelectorAll("li");
-            re_Element.forEach(function(re_element) {
-                var re_pElement = re_element.querySelectorAll("p");
-                if(re_pElement !=null){
-                    re_pElement.forEach(function(re){
-                        console.log("\t"+re.textContent);
-                    })
-                    
-                }
-            });
+function comment_extract(){
+    var elements = document.querySelectorAll('li[id^="comment_li_"]');
+    elements.forEach(function(element) {
+        var pElement = element.querySelector("p");
+        if(pElement != null){
+            console.log(pElement.textContent);
+            var reply="reply_list"+element.id.substr(10);
+            var re_Element = document.querySelector("#"+reply);
+            if(re_Element!=null){
+                re_Element = re_Element.querySelectorAll("li");
+                re_Element.forEach(function(re_element) {
+                    var re_pElement = re_element.querySelectorAll("p");
+                    if(re_pElement !=null){
+                        re_pElement.forEach(function(re){
+                            console.log("\t"+re.textContent);
+                        })
+                        
+                    }
+                });
+            }
         }
-    }
-});
+    });
+}
+comment_extract();
+const targetNode = document.querySelector("div.view_comment");
+function callback(mutationList, observer) {
+    for (const mutation of mutationList) {
+        if (mutation.type === "attributes" && mutation.attributeName === "data-comment-cnt") {
+          comment_extract();
+        }
+      }
+}
+const observerOptions = {
+ 
+  attributes: true,
+
+  // false를 지정하거나 아예 생략하여 부모 노드의 변경만 감지
+  subtree: true,
+};
+
+const observer = new MutationObserver(callback);
+observer.observe(targetNode, observerOptions);
