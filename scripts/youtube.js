111c6897be311c790997c5e4504e430a2d62ab2e
Clestar <clestar73@gmail.com>
2024-04-20 12:10:31 +0900

    youtube.js 초안

---
+++ scripts/youtube.js
@@ -0,0 +1,36 @@
+function comment_extract(){
+    var elements = document.querySelectorAll('li[id^="comment_li_"]');
+    elements.forEach(function(element) {
+        var pElement = element.querySelector("p");
+        if(pElement != null){
+            console.log(pElement.textContent);
+            var reply="reply_list"+element.id.substr(10);
+            var re_Element = document.querySelector("#"+reply);
+            if(re_Element!=null){
+                re_Element = re_Element.querySelectorAll("li");
+                re_Element.forEach(function(re_element) {
+                    var re_pElement = re_element.querySelectorAll("p");
+                    if(re_pElement !=null){
+                        re_pElement.forEach(function(re){
+                            console.log("\t"+re.textContent);
+                        })
+                        
+                    }
+                });
+            }
+        }
+    });
+}
+//comment_extract();
+const targetNode = document.querySelector("div#primary");
+function callback(mutationList, observer) {
+    console.log(mutationList);
+}
+const observerOptions = {
+  chileList: true,
+  attributes: true,
+  subtree: true,
+};
+
+const observer = new MutationObserver(callback);
+observer.observe(targetNode, observerOptions);
