121e6832edcad5ab2dcc74924886b1b248e0487a
Clestar <clestar73@gmail.com>
2024-05-27 16:15:21 +0900

    설정 수정

---
+++ scripts/instiz.js
@@ -0,0 +1,66 @@
+var commentCount = 0;
+var commentList=[];
+var option;
+function comment_extract(){
+  commentList=[];
+    var navercommentList = document.getElementsByClassName('comment_line');
+    for( var i = 0; i < navercommentList.length; i++){
+        if(navercommentList[i].classList.contains("extracted")) continue;
+        var commentString = navercommentList[i].innerText;
+        commentList.push({
+            "id":commentCount,
+            "text": commentString
+        })
+        commentCount++;
+    }
+    console.log(commentList);
+    send_message();
+}
+chrome.storage.local.get("option", function(data) {
+  option = data.option
+});
+function replace(data){
+  var navercommentList = document.getElementsByClassName('u_cbox_contents');
+  var json_idx=0;
+    for( var i = 0; i < navercommentList.length; i++){
+        if(navercommentList[i].classList.contains("extracted")) continue;
+        var origin_text = navercommentList[i].innerText;
+        navercommentList[i].setAttribute('data-origin-text',origin_text);
+        navercommentList[i].setAttribute('data-censored','true');
+        navercommentList[i].innerText = "검열된 댓글입니다"
+        console.log(data[json_idx].labelPrediction);
+        json_idx++;
+        navercommentList[i].classList.add("extracted");
+        navercommentList[i].addEventListener("click", (e) => {
+          if(e.target.getAttribute('data-censored')==='true'){
+            e.target.innerText = e.target.getAttribute('data-origin-text');
+            e.target.setAttribute('data-censored','false');
+          }
+          else{
+            e.target.innerText = "검열된 댓글입니다";
+            e.target.setAttribute('data-censored','true');
+          }
+          
+      });
+    }
+}
+function send_message(){
+    fetch('https://project-march.inha.me/api/blind', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify(commentList),
+    })
+    .then((response) => response.json())
+    .then((data) => replace(data));
+}
+chrome.runtime.onMessage.addListener((message,sender,sendResponse)=>{
+    const intervalId = setInterval(() => {
+        commentCount=0;
+        comment_extract();
+        if (commentCount > 0) {
+            clearInterval(intervalId);
+        }
+    }, 1000);
+});
