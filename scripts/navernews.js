2a412585178c323357992a05a95075851ea9203c
Clestar <clestar73@gmail.com>
2024-06-08 16:58:03 +0900

    댓글을 뭉치가 아닌 개당 보내기로 수정

--- scripts/navernews.js
+++ scripts/navernews.js
@@ -1,24 +1,71 @@
 var commentCount = 0;
 var commentList=[];
 var option;
+var idx = 0;
 function comment_extract(){
+  console.log("hi")
   commentList=[];
     var navercommentList = document.getElementsByClassName('u_cbox_contents');
     for( var i = 0; i < navercommentList.length; i++){
         if(navercommentList[i].classList.contains("extracted")) continue;
+        navercommentList[i].setAttribute('idx',idx);
         var commentString = navercommentList[i].innerText;
         commentList.push({
-            "id":commentCount,
+            "id":idx.toString(),
             "text": commentString
         })
+        comment={
+          "id":idx.toString(),
+          "text": commentString
+        }
+        idx++;
         commentCount++;
+        send_message();
     }
-    console.log(commentList);
-    send_message();
+    //console.log(commentList);
+    //send_message();
 }
 chrome.storage.local.get("option", function(data) {
   option = data.option
 });
+function replace(data){
+  var navercommentList = document.getElementsByClassName('u_cbox_contents');
+    for( var i = 0; i < navercommentList.length; i++){
+        if(navercommentList[i].classList.contains("extracted")) continue;
+        if(navercommentList[i].getAttribute('idx')!=parseInt(data.id)) continue;
+        var origin_text = navercommentList[i].innerText;
+        var blind_text="검열된 댓글입니다. by ";
+        var censor = false;
+        var prediction = data.labelPrediction;
+        for(var j = 0; j < 6; j++){
+          if(option[prediction[j].label]){
+            console.log(prediction[j].score+ " "+ option["intensity"]/100)
+            if(prediction[j].score>option["intensity"]/100){
+              blind_text+=prediction[j].label+" ";
+              censor=true;
+            }
+          }
+        }
+        if(censor){
+          navercommentList[i].setAttribute('data-origin-text',origin_text);
+          navercommentList[i].setAttribute('data-censored','true');
+          navercommentList[i].innerText = blind_text;
+          navercommentList[i].addEventListener("click", (e) => {
+            if(e.target.getAttribute('data-censored')==='true'){
+              e.target.setAttribute('data-censored',e.target.innerText);
+              e.target.innerText = e.target.getAttribute('data-origin-text');
+            }
+            else{
+              e.target.innerText = e.target.getAttribute('data-censored');
+              e.target.setAttribute('data-censored','true');
+            }
+            
+        });
+        }
+        navercommentList[i].classList.add("extracted");  
+    }
+}
+/*
 function replace(data){
   var navercommentList = document.getElementsByClassName('u_cbox_contents');
     for( var i = 0; i < navercommentList.length; i++){
@@ -55,13 +102,14 @@ function replace(data){
         navercommentList[i].classList.add("extracted");  
     }
 }
+*/
 function send_message(){
     fetch('https://project-march.inha.me/api/blind', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json;charset=utf-8',
       },
-      body: JSON.stringify(commentList),
+      body: JSON.stringify(comment),
     })
     .then((response) => response.json())
     .then((data) => replace(data));
